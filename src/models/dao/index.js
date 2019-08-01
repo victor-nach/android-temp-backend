4df4e4b337edefabc38443f8a7c3cb1b3e91ce7e
victor-nach <viheanaco@gmail.com>
2019-08-01 16:55:40 +0100

    add extra fields for to the users table

--- src/models/dao/index.js
+++ src/models/dao/index.js
@@ -2,11 +2,26 @@ import db from '../db';
 import utils from '../../utils';
 
 class DAO {
-  static async insertUser(name, address, email, userID, fingerPrint, gender) {
-    const values = [name, address, email, userID, fingerPrint, gender];
-    const query = `INSERT INTO users
-                    (name, address, email, user_id, finger_print, gender)
-                    VALUES ($1, $2, $3, $4, $5, $6) RETURNING *
+  static async insertUser(
+    name, address, email, userID, fingerPrint,
+    fingerPrintToSend, fingerPosition, gender, dateCreated,
+  ) {
+    const values = [
+      name, address, email, userID, fingerPrint,
+      fingerPrintToSend, fingerPosition, gender, dateCreated,
+    ];
+    const query = `INSERT INTO users (
+                                  name,
+                                  address,
+                                  email,
+                                  user_id,
+                                  finger_print,
+                                  finger_print_to_send,
+                                  finger_position,
+                                  gender,
+                                  date_created
+                                  )
+                    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *
                     `;
     const { rows } = await db.query(query, values);
     return utils.camelCased(rows[0]);
